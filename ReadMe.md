e9819233b39cea4494ad2bc3e77aff0a5bcdfd1e
A. H. M. Rezaul Karim <rezaulkarim@iut-dhaka.edu>
2022-09-30 23:52:42 +0600

    Debugging and apk added

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,43 @@
-# Xylophone App
+<h1 align="center">
+<img src="https://github.com/AHMRezaul/Xylophone-App/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"  width="200" height="200" />
+<br> Xylophone App </br>
+</h1>
+
+<h4 align="center">
+A mobile application developed using flutter technologies.
+APK installation link: https://github.com/AHMRezaul/Xylophone-App/blob/main/xylophone/splits/base-arm64_v8a.apk
+</h4>
+
+## Overview
+A mobile application created using flutter app development technologies. This application can be run on any platforms (android or iOS).
+The application simulates the xylophone, a musical instrument and can be played by pressing the 7 different keys displayed on screen.
+The app has a simple design and uses very little computational power.
+
+## Tools:
+The application is developed using **Flutter Technologies** and **Dart** language. Flutter is a technology developed by Google that can create both an **Android** and an **iOS** application simultaneously.
+The following tools and software were used to develop the application:
+1. Flutter
+2. Dart
+3. Android Studio
+4. Emulator
+
+### Technical details:
+Flutter version: 2.8.1
+Dart SDK version: 2.1.5
+AudioPlayers (Dart package): 0.17.4
+
+## Installation:
+To run the project, clone the repository and open it using **Android Studio**.
+Open the **pubspec.yaml** file and run the following command.
+```
+Pub get
+```
+This will download all the dependencies and packages required for the project. 
+Next, run the emulator and finally build and run the application on the emulator.
+
+To install the application, download from the link: https://github.com/AHMRezaul/Xylophone-App/blob/main/xylophone/splits/base-arm64_v8a.apk
+
+## App Preview:
+Xylophone:
+![screenshot](https://github.com/AHMRezaul/Xylophone-App/blob/main/Screenshot.png)
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -5,7 +6,8 @@ void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   void playSound(int soundNumber) {
-    final player = AudioCache();
+    final player = new AudioCache(fixedPlayer: AudioPlayer());
+    player.fixedPlayer.stop();
     player.play('note$soundNumber.wav');
   }
 
@@ -23,20 +25,21 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                buildKey(color: Colors.red, soundNumber: 1),
-                buildKey(color: Colors.orange, soundNumber: 2),
-                buildKey(color: Colors.yellow, soundNumber: 3),
-                buildKey(color: Colors.green, soundNumber: 4),
-                buildKey(color: Colors.teal, soundNumber: 5),
-                buildKey(color: Colors.blue, soundNumber: 6),
-                buildKey(color: Colors.purple, soundNumber: 7),
-              ],
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red.shade900, soundNumber: 1),
+              buildKey(color: Colors.orange.shade900, soundNumber: 2),
+              buildKey(color: Colors.yellow.shade900, soundNumber: 3),
+              buildKey(color: Colors.green.shade900, soundNumber: 4),
+              buildKey(color: Colors.teal.shade300, soundNumber: 5),
+              buildKey(color: Colors.blue.shade900, soundNumber: 6),
+              buildKey(color: Colors.purple.shade900, soundNumber: 7),
+            ],
           ),
         ),
       ),
